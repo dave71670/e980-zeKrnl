397f9da86b51af377e186309e0234830aeb56fac
Igor <c.igor@gmx.com>
2015-09-04 12:45:01 +0200

    ondemandPLUS: adjust to OGPro's CPU

--- drivers/cpufreq/cpufreq_ondemandplus.c
+++ drivers/cpufreq/cpufreq_ondemandplus.c
@@ -31,8 +31,8 @@
  * 	drivers/cpufreq/cpufreq_ondemand.c
  *
  * 	Copyright (C)  2001 Russell King
- *           	  (C)  2003 Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>.
- *                     	    Jun Nakajima <jun.nakajima@intel.com>
+ *            (C)  2003 Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>.
+ *                      Jun Nakajima <jun.nakajima@intel.com>
  *
  * 	This program is free software; you can redistribute it and/or modify
  * 	it under the terms of the GNU General Public License version 2 as
@@ -89,25 +89,25 @@ static spinlock_t speedchange_cpumask_lock;
 #define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
 static unsigned long timer_rate;
 
-#define DEFAULT_UP_THRESHOLD 95
+#define DEFAULT_UP_THRESHOLD 70
 static unsigned long up_threshold;
 
-#define DEFAULT_DOWN_DIFFERENTIAL 62
+#define DEFAULT_DOWN_DIFFERENTIAL 20
 static unsigned long down_differential;
 
-#define DEFAULT_MIN_FREQ 162000
+#define DEFAULT_MIN_FREQ 384000
 static u64 allowed_min;
 
-#define DEFAULT_MAX_FREQ 1512000
+#define DEFAULT_MAX_FREQ 1728000
 static u64 allowed_max;
 
-#define DEFAULT_INTER_HIFREQ 1350000
+#define DEFAULT_INTER_HIFREQ 1242000
 static u64 inter_hifreq;
 
 #define DEFAULT_INTER_LOFREQ 1026000
 static u64 inter_lofreq;
 
-#define SUSPEND_FREQ 702000
+#define SUSPEND_FREQ 486000
 static u64 suspend_frequency;
 
 #define DEFAULT_INTER_STAYCYCLES 2
@@ -116,7 +116,7 @@ static unsigned long inter_staycycles;
 #define DEFAULT_STAYCYCLES_RESETFREQ 486000
 static u64 staycycles_resetfreq;
 
-#define DEFAULT_IO_IS_BUSY 2
+#define DEFAULT_IO_IS_BUSY 0
 static unsigned int io_is_busy;
 
 /*
